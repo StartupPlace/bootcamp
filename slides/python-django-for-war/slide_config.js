a55482ae9261f6383f9ab72aac7e409966ad04a2
geoom <gmejia.fisi@gmail.com>
2014-04-18 12:41:29 -0500

    python-django-for-war slides has been added again

---
+++ slides/python-django-for-war/slide_config.js
@@ -0,0 +1,32 @@
+var SLIDE_CONFIG = {
+  settings: {
+    title: 'Python/Django for war',
+    subtitle: 'what you need to build agile and robust projects',
+    // eventInfo: {
+    //  title: 'Bootcamp trainings - python/django',
+    //  date: '20/3/2014'
+    // },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/favicon.ico',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Authors info
+  presenters: [
+    {
+      name: 'George Mejia',
+      company: 'Software developer<br>StartupPlace',
+      twitter: '@geoome',
+      github: 'http://github.com/geoom',
+      email: 'hi.geoom@gmail.com',
+    },
+  ]
+};
+
